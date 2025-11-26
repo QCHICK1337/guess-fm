f8e59b9a0be2536accc9d790f9ae3470360c0f06
QCHICK <35234463+QCHICK1337@users.noreply.github.com>
2025-11-26 12:14:18 +0100

    Add 'Next Song' flow, improve code quality and UX

--- app.js
+++ app.js
@@ -6,9 +6,22 @@ const guessInput = document.getElementById("guess-input");
 const songSuggestions = document.getElementById("song-suggestions");
 const submitBtn = document.getElementById("submit-btn");
 const feedbackTxt = document.getElementById("feedback-txt");
+const nextBtn = document.getElementById("next-btn");
 
 let currentSong = null;
 let allSongs = [];
+
+audioPlayer.volume = 0.5;
+
+function playRandomSong() {
+  audioPlayer.pause();
+
+  const randomIndex = Math.floor(Math.random() * allSongs.length);
+  currentSong = allSongs[randomIndex];
+
+  audioPlayer.src = currentSong.previewUrl;
+  audioPlayer.play();
+}
 
 searchBtn.addEventListener("click", () => {
   const artistName = searchInput.value;
@@ -24,24 +37,23 @@ searchBtn.addEventListener("click", () => {
     .then((response) => response.json())
     .then((data) => {
       allSongs = data.results;
-      const randomIndex = Math.floor(Math.random() * allSongs.length);
 
       songSuggestions.innerHTML = "";
       allSongs.forEach((song) => {
-        const songSuggestionString = document.createElement("option");
-        songSuggestionString.value = song.trackName;
-        songSuggestions.appendChild(songSuggestionString);
+        const songSuggestionOption = document.createElement("option");
+        songSuggestionOption.value = song.trackName;
+        songSuggestions.appendChild(songSuggestionOption);
       });
 
-      currentSong = allSongs[randomIndex];
-
-      audioPlayer.src = currentSong.previewUrl;
-      audioPlayer.play();
-
+      playRandomSong();
+      searchInput.style.display = "none";
+      searchBtn.style.display = "none";
       gameContainer.style.display = "flex";
+      guessInput.focus();
     })
     .catch((error) => {
       console.error("Error fetching data:", error);
+      alert("Error fetching data:", error);
     });
 });
 
@@ -52,7 +64,15 @@ submitBtn.addEventListener("click", () => {
     currentSong.trackName.toLowerCase().trim()
   ) {
     feedbackTxt.textContent = "Correct!";
+    nextBtn.style.display = "flex";
   } else {
     feedbackTxt.textContent = "Wrong!";
   }
+});
+
+nextBtn.addEventListener("click", () => {
+  playRandomSong();
+  nextBtn.style.display = "none";
+  feedbackTxt.textContent = "";
+  guessInput.value = "";
 });
