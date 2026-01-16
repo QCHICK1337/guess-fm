ce6236298e14a543944eaa09ad3a2dddd6d5c5ce
QCHICK <35234463+QCHICK1337@users.noreply.github.com>
2026-01-16 13:47:48 +0100

    Add eslint setup

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    ...js.configs.recommended,
+
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      ...js.configs.recommended.languageOptions,
+      globals: globals.browser,
+    },
+    extends: ["prettier"],
+  },
+]);
